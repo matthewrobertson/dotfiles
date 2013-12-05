ac06a22b0d847dad503404c528dd7626ed5a33a4
Matthew Robertson <mrobertson@frostys-iphone4.thefacebook.com>
2013-12-05 00:55:00 -0800

    removed old stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-  --exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+  --exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude ".gitmodules" \
+  -av --no-perms . ~
 source ~/.bash_profile
