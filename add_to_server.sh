b5a0bcd59efc127d94a0040843fe2d00aa083b03
Matthew Robertson <matthewrobertson03@gmail.com>
2013-12-07 23:37:17 -0800

    added some ctags stuff for fb and a file to rsync to a server

--- add_to_server.sh
+++ add_to_server.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+
 rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
   --exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude ".gitmodules" \
-  --exclude "add_to_server.sh" -av --no-perms . ~
+  --exclude "add_to_server.sh" -av --no-perms . $1:~
 
 if [[ -f ~/.bash_profile ]]; then
   source ~/.bash_profile
